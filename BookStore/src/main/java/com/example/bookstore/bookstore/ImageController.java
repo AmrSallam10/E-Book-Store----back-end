5ea428f2fa722ccf0e098532260bc6051da92549
Omar Hatem <omarhatem2002@aucegypt.edu>
2022-08-24 14:09:17 +0200

    cleans code

--- BookStore/src/main/java/com/example/bookstore/bookstore/ImageController.java
+++ BookStore/src/main/java/com/example/bookstore/bookstore/ImageController.java
@@ -10,7 +10,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.mongodb.lang.NonNull;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.validation.Valid;
 
@@ -39,45 +38,12 @@ public class ImageController {
             book.setImage(new Binary(file.getBytes()));
             book.setImageType(file.getContentType());
 
-            Book savedFile = imageRepository.save(book);
-            String url = "http://localhost:8080/api/bookstore/images" + savedFile.getId();
+            imageRepository.save(book);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
-
-//    @GetMapping
-//    public ResponseEntity<List<Book>> getAllBooks() {
-//        List<Book> books = imageRepository.findAll();
-//        return new ResponseEntity<>(books, HttpStatus.OK);
-//    }
-
-//    @GetMapping(path ={"/id"})
-//    public ResponseEntity<Book> getBookById(String id) {
-//        Book book = imageRepository.findById(id).orElse(null);
-//        if (book.equals(null)) {
-//            return new ResponseEntity<>(book, HttpStatus.BAD_REQUEST);
-//
-//        } else {
-//            return new ResponseEntity<>(book, HttpStatus.BAD_REQUEST);
-//        }
-//    }
-
-//    @PutMapping(path = {"/id"})
-//    @ResponseBody
-//    public void updateImage(@PathVariable("id") String id,
-//                            @RequestParam Integer bookQuantity) {
-//
-////        String fileName = file.getOriginalFilename();
-//            Book book = imageRepository.findById(id).orElse(null);
-//            book.setBookQuantity(bookQuantity);
-//
-//            imageRepository.save(book);
-//    }
-
-
-
 
 }
